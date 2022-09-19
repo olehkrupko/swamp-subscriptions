69024a7ad5e58a0399899caf4fac9fe71054637e
Oleh Krupko <oleh.krupko@gmail.com>
2022-09-20 00:45:52 +0300

    safdasf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 export default function App() {
     return (
         <div>
-            <Navigation /> 
+            <Navigation />
             <Outlet />
         </div>
     );
