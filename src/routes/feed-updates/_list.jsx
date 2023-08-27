b2f6d9aa5390c70a1190a461051c7e3de342eb79
Oleh Krupko <oleh.krupko@gmail.com>
2023-08-28 02:01:41 +0300

    feed update list: refactored to component

---
+++ src/routes/feed-updates/_list.jsx
@@ -0,0 +1,51 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useNavigate } from "react-router-dom";
+
+
+function openInNewTab(href) {
+    window.open(href, '_blank')
+        .focus();
+}
+
+export default function FeedUpdatesList(props) {
+    const navigate = useNavigate();
+
+    console.log(typeof props.feedUpdates, props.feedUpdates)
+    return(
+        <ListGroup>
+            {props.feedUpdates.map((update) => (
+                <ListGroup.Item>
+                    <span
+                        style={{
+                            color: 'red',
+                            fontWeight: 500,
+                        }}
+                    >
+                        »
+                    </span>
+                    &nbsp;
+                    <span
+                        style={{
+                            cursor: "pointer",
+                            fontWeight: "bold"
+                        }}
+                        onClick={() => openInNewTab( update.href )}
+                    >
+                        {update.name}
+                    </span>
+                    &nbsp;(by&nbsp;
+                    <span
+                        style={{
+                            cursor: "pointer",
+                            fontWeight: "bolder"
+                        }}
+                        onClick={() => navigate("/feeds/"+ update.feed_id)}
+                    >
+                        { update.feed_data.title }
+                    </span>
+                    &nbsp;on { update.datetime })
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    )
+}
