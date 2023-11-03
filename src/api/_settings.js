3dc3a87bb4ff69a85b4dffb49a08fa19d4a0881a
Oleh Krupko <oleh.krupko@gmail.com>
2023-11-03 11:53:53 +0200

    Revert "Test local URL usage"
    This reverts commit 441e5e19699908171c75357baa0b6fc83b93ba2a.

--- src/api/_settings.js
+++ src/api/_settings.js
@@ -1,2 +1,1 @@
-// export const SWAMP_API_BASE_URL  = 'http://192.168.0.155:30010'
-export const SWAMP_API_BASE_URL  = 'http://127.0.0.1:30010'
+export const SWAMP_API_BASE_URL  = 'http://192.168.0.155:30010'
