5096307c039a3f16582ab73196bc803b6eae0d27
Oleh Krupko <oleh.krupko@gmail.com>
2023-11-25 00:32:33 +0200

    Switching to proper server

--- src/api/_settings.js
+++ src/api/_settings.js
@@ -1,3 +1,3 @@
-export const SWAMP_API = 'http://127.0.0.1:30010'  // local
+// export const SWAMP_API = 'http://127.0.0.1:30010'  // local
 // export const SWAMP_API = 'http://192.168.0.222:30010'  // Mac
-// export const SWAMP_API = 'http://192.168.0.155:30010'  // Synology
+export const SWAMP_API = 'http://192.168.0.155:30010'  // Synology
