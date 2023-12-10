9b080d938d2762864be02d47afbfefc51cd6f9c6
Oleh Krupko <oleh.krupko@gmail.com>
2023-12-10 23:17:10 +0200

    Init update kwargs

--- src/routes/feed-updates/list.jsx
+++ src/routes/feed-updates/list.jsx
@@ -1,12 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 
 import FeedUpdatesApi from '../../api/feed-updates';
 import FeedUpdatesList from './_list';
 
   
 export default function FeedsList() {
+    const LOAD_LESS = 300;
+    const LOAD_MORE = 1500;
+
     const [feedUpdates, setFeedUpdates] = useState([]);
-    const [kwargs, setKwargs] = useState({limit: 3000});
+    const [kwargs, setKwargs] = useState({limit: LOAD_LESS});
 
     useEffect(() => {
         FeedUpdatesApi.getFeedUpdates(kwargs)
@@ -27,6 +33,34 @@ export default function FeedsList() {
     return (
         <main>
             <h1>Updates</h1>
+            <ButtonGroup>
+                <Button
+                    variant="secondary"
+                    onClick={() => {
+                        setKwargs({
+                            ...kwargs,
+                            ...{
+                                limit: LOAD_LESS,
+                            }
+                        });
+                    }}
+                >
+                    Load: LESS
+                </Button>
+                <Button
+                    variant="secondary"
+                    onClick={() => {
+                        setKwargs({
+                            ...kwargs,
+                            ...{
+                                limit: LOAD_MORE,
+                            }
+                        });
+                    }}
+                >
+                    Load: MORE
+                </Button>
+            </ButtonGroup>
             {feedUpdates &&
                 <FeedUpdatesList
                     feedUpdates={feedUpdates}
