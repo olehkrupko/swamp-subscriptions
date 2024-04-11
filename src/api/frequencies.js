289a158764408495813ab3d774f6497203ccdc1b
Oleh Krupko <oleh.krupko@gmail.com>
2024-04-11 22:23:38 +0300

    Frequency: API URL updated

--- src/api/frequencies.js
+++ src/api/frequencies.js
@@ -3,7 +3,7 @@ import { SWAMP_API } from './_settings.js';
 
 export default class FrequencyApi {
     static async getFrequencies() {
-        return fetch(`${SWAMP_API}/frequencies/`)
+        return fetch(`${SWAMP_API}/frequency/`)
             .then(res => res.json())
     }
 }
