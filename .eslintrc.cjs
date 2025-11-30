2e86c2a6f608f92aa3bc462548ef27c3ed693898
Oleh Krupko <oleh.krupko@gmail.com>
2025-11-30 16:15:48 +0200

    Actions: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  plugins: ['filenames'],
+  overrides: [
+    {
+      files: ['src/components/**/*.{js,jsx}'],
+      rules: {
+        // Enforce PascalCase for component filenames (basename only)
+        'filenames/match-regex': ['error', '^[A-Z][a-zA-Z0-9]*$']
+      }
+    }
+  ]
+};
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['filenames'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.js'] }],
+    // Enforce PascalCase for component filenames. Allows index.js files.
+    'filenames/match-regex': [
+      'error',
+      '^[A-Z][a-zA-Z0-9]*(?:[A-Z][a-zA-Z0-9]*)?$|^index$|^index\.(js|jsx)$',
+      true,
+    ],
+  },
+};
